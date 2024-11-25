aed3980317d0e9e9046071a7c95e2bc7f13230ae
Paulo <paulo.kravchuk+github@studio.unibo.it>
2024-11-25 12:20:25 +0100

    added second exercise

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,46 +1,90 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    // private static final int MIN = 0;
+    // private static final int MAX = 100;
+    // private static final int ATTEMPTS = 10;
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
 
     /**
      * @param views
-     *            the views to attach
+     *              the views to attach
      */
-    public DrawNumberApp(final DrawNumberView... views) {
+    public DrawNumberApp(final String configFileName, final DrawNumberView... views) {
         /*
          * Side-effect proof
          */
         this.views = Arrays.asList(Arrays.copyOf(views, views.length));
-        for (final DrawNumberView view: views) {
+        for (final DrawNumberView view : views) {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+
+        final Configuration.Builder builder = new Configuration.Builder();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(ClassLoader.getSystemResourceAsStream(configFileName)))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] parts = line.split(":");
+
+                if (parts.length == 2) {
+                    String value = parts[1].trim();
+
+                    switch (parts[0]) {
+                        case "minimum":
+                            builder.setMin(Integer.parseInt(value));
+                            break;
+                        case "maximum":
+                            builder.setMax(Integer.parseInt(value));
+                            break;
+                        case "attempts":
+                            builder.setAttempts(Integer.parseInt(value));
+                            break;
+                        default:
+                            break; // future settings in case they need to be parsed differently
+                    }
+                } else {
+                    displayError("Malformed setting, unable to parse: " + line);
+                }
+
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            displayError(e.getMessage());
+        }
+
+        final Configuration config = builder.build();
+        this.model = new DrawNumberImpl(config.getMin(), config.getMax(), config.getAttempts());
     }
 
     @Override
     public void newAttempt(final int n) {
         try {
             final DrawResult result = model.attempt(n);
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.result(result);
             }
         } catch (IllegalArgumentException e) {
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.numberIncorrect();
             }
+        }
+    }
+
+    private void displayError(final String err) {
+        for (final DrawNumberView view : views) {
+            view.displayError(err);
         }
     }
 
@@ -62,11 +106,16 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
 
     /**
      * @param args
-     *            ignored
-     * @throws FileNotFoundException 
+     *             ignored
+     * @throws FileNotFoundException
      */
     public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+        new DrawNumberApp(
+                "config.yml",
+                new DrawNumberViewImpl(),
+                new DrawNumberViewImpl(),
+                new PrintStreamView(System.out),
+                new PrintStreamView("output.log"));
     }
 
 }
